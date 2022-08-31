da950d9275dd159d8003e7485de9e3e804b77cfc
ANKITSAINI3323 <ankitsaini8720@gmail.com>
2022-08-31 18:02:35 +0530

    Create greater1.c
    ok

---
+++ classwork/greater1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{	int a,b;
+	printf("Enter A:");
+	scanf("%d",&a);
+	printf("Enter B :");
+	scanf("%d",&b);
+	
+
+     if (a>b)
+	 { 
+	 printf("a is greater");
+	 
+	 }
+	 else
+	 {
+	 	printf("b is greater");
+	 }
+}
