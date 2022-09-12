3cbe0ebc7e46de43c1cae1117088beca909c13a9
ANKITSAINI3323 <ankitsaini8720@gmail.com>
2022-09-12 21:15:14 +0530

    ok

--- C assignment/square_cube.c
+++ C assignment/square_cube.c
@@ -7,7 +7,7 @@ void main()
   printf("Enter a number");
     scanf("%d",&n);   
     
-    printf("\nSquare number %d : %d ",n,n*n);
+    printf("\nSquare number %d: %d ",n,n*n);
     printf("\nCube number %d : %d",n,n*n*n);
 	
 	
